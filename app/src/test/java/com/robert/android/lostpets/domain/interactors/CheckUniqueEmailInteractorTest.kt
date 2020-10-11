cde638ec74579a3ad9d6654fc797edc08e1afd27
Robert Ene <robertene1994@gmail.com>
2020-10-11 20:40:02 +0300

    CI/CD test improvements

--- app/src/test/java/com/robert/android/lostpets/domain/interactors/CheckUniqueEmailInteractorTest.kt
+++ app/src/test/java/com/robert/android/lostpets/domain/interactors/CheckUniqueEmailInteractorTest.kt
@@ -35,7 +35,7 @@ import org.mockito.junit.MockitoJUnitRunner
 class CheckUniqueEmailInteractorTest {
 
     private companion object {
-        const val SERVER_PORT = 1000
+        const val SERVER_PORT = 64395
         const val RESPONSE_TIMEOUT = 3000L
     }
 
